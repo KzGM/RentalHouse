7eef42e150ced379db3cfe71e4af7a88150e1d7c
WINNINGANDLOSING <106044627+WINNINGANDLOSING@users.noreply.github.com>
2022-12-15 21:01:38 +0700

    Chỉnh homepage 1 tí

--- homepage.php
+++ homepage.php
@@ -1,11 +1,24 @@
+
 <!DOCTYPE html>
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+require ('db.php');
+require_once "Controller.php";
+session_start();
+try {
+  $Email = $_SESSION['Email'];
+}
+catch(Exception $e){
+
+}
+?>
+
+
 <html lang="english">
   <head>
-    <title>exported project</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta charset="utf-8" />
-    <meta property="twitter:card" content="summary_large_image" />
-
+    <title>Home</title>
+  
+    
     <style data-tag="reset-style-sheet">
       html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}html { scroll-behavior: smooth  }
     </style>
@@ -42,16 +55,36 @@
       href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&amp;display=swap"
       data-tag="font"
     />
-    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="./style.css" /> 
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>House Detail</Details></title>
+    <link rel="stylesheet" href="./style1.css" />
+
+    <title> Rent House </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <scrip scr="https://ajax.com.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"> </script>
+    <scrip scr="js/bootstrap.min.js"> </script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+
+
   </head>
   <body>
     
     <div>
-      <link href="./desktop1.css" rel="stylesheet" />
+      <link href="./desktop2.css" rel="stylesheet" />
 
       <div class="desktop1-container">
         <div class="desktop1-desktop1">
-          <div class="desktop1-navbar">
+          <div class="desktop1-navbar"> 
             <img
               alt="Vector2640"
               src="public/playground_assets/vector2640-tqc.svg"
@@ -61,22 +94,49 @@
               <div class="desktop1-navbar1">`
                 <div class="topnav">
                   <a class="active" href="#home">Home</a>
-                  <a href="#about">About</a>
-                  <a href="#contact">Contact</a>
-                  <input type="text" placeholder="Search..">
+                  <a href="aboutUs.php">About</a>
+                  <a href="contact.php">Contact</a>
+                  
                 </div>
 
                 <div class="topnav">
                   
-                  <a href="Sign_up.php">Sign up</a>
-                  <a href="Sign_in.php">Sign in</a>
+                  
+                  
+                  <a href="Rent.php"> Rent </a>
                   
                 </div>
 
-                
-              </div>
+                <div style="padding: 1px 20px;">
+                  <form action="homepage.php" method="post" enctype="multipart/form-data">
+                  <input name="search" type="text"  placeholder="Search for apartments..." style="border:3px solid black; height:45px; width: 250px;" > </input>
+                  <input style = "border:1px solid black; border-radius: 10px; background:#FF6F61; height:45px; width:60px" type="submit" name="submit" value = "Search">  </input>
+                  </form>
+                </div>
+
+                <div style="padding: 1px 10px; margin-left:40px; font-size:18px; font-family:Arial, Helvetica, sans-serif; font-weight: bold;">
+                <?php
+                if ($Email == "") {
+                  echo '<div class = "topnav">';
+                  echo '<a href = "Sign_up.php"> Sign up  </a>';
+                  echo '</div>';
+                  echo '</div>';
+                }
+                else{
+                  echo "<p style='text-align:center'>Welcome</p><p style='color:red'>" . $Email . "</p>";
+                  echo "</div>";
+                  echo '<div class="topnav">';
+                  echo '<a href="Sign_out.php">Sign out</a>';
+                  echo '</div>';
+                }
+                ?>
+
             </div>
+           
           </div>
+
+          
+
           <div class="desktop1-group244">
             <div class="desktop1-headerpicture">
               <img
