4f50ba1a650b37b70275709ef4a7dfd5767f68dd
WINNINGANDLOSING <106044627+WINNINGANDLOSING@users.noreply.github.com>
2022-12-16 13:13:46 +0700

    Bỏ trong folder htdocs rồi chạy
     cài xaamp trước (bat buoc)

--- ehome.sql
+++ ehome.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2022 at 09:16 PM
+-- Generation Time: Dec 16, 2022 at 07:10 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -32,8 +32,40 @@ CREATE TABLE `houseinfo` (
   `HouseID` int(11) UNSIGNED NOT NULL,
   `Address` varchar(50) DEFAULT NULL,
   `Price` int(11) DEFAULT NULL,
-  `Rented` bit(1) DEFAULT NULL
+  `image` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  `Title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `houses`
+--
+
+CREATE TABLE `houses` (
+  `HID` int(11) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `HAddress` varchar(255) NOT NULL,
+  `Contact` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  `img0` varchar(255) NOT NULL,
+  `img1` varchar(255) NOT NULL,
+  `img2` varchar(255) NOT NULL,
+  `img3` varchar(255) NOT NULL,
+  `img4` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+
+--
+-- Dumping data for table `houses`
+--
+
+INSERT INTO `houses` (`HID`, `Title`, `Price`, `HAddress`, `Contact`, `Description`, `img0`, `img1`, `img2`, `img3`, `img4`) VALUES
+(1, 'Apartment with kitchen and attic', '8500000.00', 'Da Nang', '0914356650', '-', '/DuyDat/img/nha1_00.png', '/DuyDat/img/nha1_01.png', '/DuyDat/img/nha1_02.png', '/DuyDat/img/nha1_03.png', '/DuyDat/img/nha1_04.png'),
+(2, 'Apartment with full household appliance and a bathroom', '6879000.00', 'TP HCM', '0977757533', '-', '/DuyDat/img/nha2_00.png', '/DuyDat/img/nha2_01.png', '/DuyDat/img/nha2_02.png', '/DuyDat/img/nha2_03.png', '/DuyDat/img/nha2_04.png'),
+(3, 'Boarding house with 1 bathroom and small kitchen corner', '6000000.00', 'TP HCM', '0333884567', '-', '/DuyDat/img/nha3_00.png', '/DuyDat/img/nha3_01.png', '/DuyDat/img/nha3_02.png', '/DuyDat/img/nha3_03.png', '/DuyDat/img/nha3_04.png'),
+(4, 'Boarding house with small attic and a bathroom', '4250000.00', 'Da Nang', '0974742323', '-', '/DuyDat/img/nha4_00.png', '/DuyDat/img/nha4_01.png', '/DuyDat/img/nha4_02.png', '/DuyDat/img/nha4_03.png', '/DuyDat/img/nha4_04.png');
 
 -- --------------------------------------------------------
 
@@ -65,10 +97,11 @@ CREATE TABLE `userlogin` (
 --
 
 INSERT INTO `userlogin` (`UserID`, `Email`, `Password`, `Code`) VALUES
-(8, 'malwarefrondo@gmail.com', 'zxc', '59920'),
-(9, 'jizzthekid4@gmail.com', '123', '0'),
-(10, 'tranvaa@gmail.com', '123', '0'),
-(11, 'tranvag', '123', '0');
+(12, 'khoalieudang@gmail.com', '123', ''),
+(13, 'aaa@gmail.com', '123', ''),
+(14, 'khoalieudang1@gmail.com', '123', ''),
+(15, 'testscrum7@gmail.com', '123', '83759'),
+(16, 'aa1@gmail.com', '123', '');
 
 --
 -- Indexes for dumped tables
@@ -80,6 +113,12 @@ INSERT INTO `userlogin` (`UserID`, `Email`, `Password`, `Code`) VALUES
 ALTER TABLE `houseinfo`
   ADD PRIMARY KEY (`HouseID`),
   ADD KEY `OwnerID` (`OwnerID`);
+
+--
+-- Indexes for table `houses`
+--
+ALTER TABLE `houses`
+  ADD PRIMARY KEY (`HID`);
 
 --
 -- Indexes for table `ownerlogin`
@@ -104,6 +143,12 @@ ALTER TABLE `houseinfo`
   MODIFY `HouseID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `houses`
+--
+ALTER TABLE `houses`
+  MODIFY `HID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `ownerlogin`
 --
 ALTER TABLE `ownerlogin`
@@ -113,7 +158,7 @@ ALTER TABLE `ownerlogin`
 -- AUTO_INCREMENT for table `userlogin`
 --
 ALTER TABLE `userlogin`
-  MODIFY `UserID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `UserID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
